45f1e8eb4b0b44b74a054b4ef67d5d6062f91fb3
ismailjacoby <ismail.jacoby.com>
2025-07-07 16:22:14 +0200

    Project Init

---
+++ music-ecommerce-api/src/main/java/com/ismailjacoby/musicecommerceapi/MusicEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ismailjacoby.musicecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicEcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicEcommerceApiApplication.class, args);
+    }
+
+}
---
+++ music-ecommerce-api/src/test/java/com/ismailjacoby/musicecommerceapi/MusicEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismailjacoby.musicecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicEcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
