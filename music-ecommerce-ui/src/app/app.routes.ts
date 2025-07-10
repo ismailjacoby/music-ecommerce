a44e459eb655de33915af3db794b1772140fc621
Ismail <ismail.jacoby@gmail.com>
2025-07-10 18:27:00 +0200

    Frontend: Homepage & Footer created

--- music-ecommerce-ui/src/app/app.routes.ts
+++ music-ecommerce-ui/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import {Home} from './components/home/home';
 
 export const routes: Routes = [
+  {path: '', component: Home},
   { path: 'auth', loadChildren: () =>
       import("./components/auth/auth.routes").then((routes)=> routes.authRoutes)}
 ];
--- music-ecommerce-ui/src/app/app.ts
+++ music-ecommerce-ui/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TopNavbar} from './components/navbar/top-navbar/top-navbar';
 import {CategoryNavbar} from './components/navbar/category-navbar/category-navbar';
+import {Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopNavbar, CategoryNavbar],
+  imports: [RouterOutlet, TopNavbar, CategoryNavbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
